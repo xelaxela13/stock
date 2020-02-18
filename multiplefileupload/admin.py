697224cff12a36a9357a9de543bcfecaa8de0013
Alex <xelaxela13@gmail.com>
2020-02-18 18:21:37 +0200

    multiple files uploads

--- multiplefileupload/admin.py
+++ multiplefileupload/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .forms.admin_forms import ImagesGalleryInlineForm
+from .forms.admin_forms import ImagesGalleryForm
 from .models import Image, ImagesGallery
 
 
@@ -9,6 +9,7 @@ from .models import Image, ImagesGallery
 class ImageAdmin(admin.ModelAdmin):
     fields = ('file', 'slug', 'thumbnail', 'snapshot', 'user', 'deleted', 'show', 'create_at')
     readonly_fields = ('thumbnail', 'snapshot', 'user', 'create_at')
+    list_display = ('snapshot', 'create_at')
 
     def save_model(self, request, obj, form, change):
         if obj and not obj.user:
@@ -21,18 +22,21 @@ class ImageAdmin(admin.ModelAdmin):
 
 class ImagesGalleryInline(admin.TabularInline):
     model = Image
-    extra = 1
-    form = ImagesGalleryInlineForm
+    extra = 0
 
 
 @admin.register(ImagesGallery)
 class ImagesGalleryAdmin(admin.ModelAdmin):
-    fields = ('gallery_name', )
+    fields = ('gallery_name', 'images')
+    form = ImagesGalleryForm
     inlines = (ImagesGalleryInline, )
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
-        st()
-        for key, images in request.FILES.items():
-            pass
 
+        images = []
+        for image in request.FILES.getlist('images'):
+            images.append(Image(file=image, user=request.user, gallery=obj))
+
+        if images:
+            Image.objects.bulk_create(images)
--- multiplefileupload/forms/admin_forms.py
+++ multiplefileupload/forms/admin_forms.py
@@ -1,8 +1,5 @@
 from django import forms
 
 
-class ImagesGalleryInlineForm(forms.ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['file'].widget.attrs = {'multiple': 'true'}
+class ImagesGalleryForm(forms.ModelForm):
+    images = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': 'true'}))
