ef1cd467c002461a69f83cd714ca500797d8c756
xela <xelaxela13@gmail.com>
2020-03-14 22:44:16 +0200

    styles

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ class Home(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        gallery = ImagesGallery.get_images_from_gallery('boxes')
-        context['boxes_images'] = gallery.values_list('file', flat=True) if gallery else []
+        for gallery in ('boxes', 'certifications', 'stabilizators'):
+            images = ImagesGallery.get_images_from_gallery(gallery)
+            context[gallery] = images.values_list('file', flat=True) if images else []
         return context
