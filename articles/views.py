193166a14ec8a4ad81b4108ffcc1bd8a73d1862b
xela <xelaxela13@gmail.com>
2020-07-19 13:46:31 +0300

    articles app

--- articles/views.py
+++ articles/views.py
@@ -10,3 +10,9 @@ class ArticleDetailView(DetailView):
 class ArticleListView(ListView):
     model = Article
     paginate_by = 6
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'half_page': str(self.paginate_by // 2)})
+        return context
+
