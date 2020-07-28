84cbc09c990229b624111f964fbc94d9f55ccf23
Alex <xelaxela13@gmail.com>
2020-07-28 11:30:39 +0300

    style and article view_count

--- articles/views.py
+++ articles/views.py
@@ -6,6 +6,14 @@ from articles.models import Article
 class ArticleDetailView(DetailView):
     model = Article
 
+    def get(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj and not request.session.get(f'viewed_{obj.pk}'):
+            obj.view_count += 1
+            obj.save(update_fields=('view_count', ))
+            request.session.setdefault(f'viewed_{obj.pk}', True)
+        return super().get(request, *args, **kwargs)
+
 
 class ArticleListView(ListView):
     model = Article
@@ -15,4 +23,3 @@ class ArticleListView(ListView):
         context = super().get_context_data(**kwargs)
         context.update({'half_page': str(self.paginate_by // 2)})
         return context
-
